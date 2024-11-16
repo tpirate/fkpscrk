505fbe4ee7213611b621fd05204a86c17d35ef57
Fikret Gurbuz <70349622+tpirate@users.noreply.github.com>
2024-11-16 16:35:04 +0300

    changes to main file

--- fkpscrk.py
+++ fkpscrk.py
@@ -24,19 +24,44 @@ def fourdigits(text, f):
                     f.write(text + digits[i] + digits[j] + digits[x] + digits[y]+ "\n")
                     f.write(digits[i] + digits[j] + digits[x] + digits[y] + text + "\n")                
 
+def addbirth(text, f):
+    # bd bm by + sy
+    # bm bd by + sy
+    # by by
+    # bd bm bd bm
+    # bm bd bm bd
+    bd = argv[3][0] + argv[3][1]
+    bm = argv[3][2] + argv[3][3]
+    by = argv[3][4] + argv[3][5] + argv[3][6] + argv[3][7]
+    bys = argv[3][6] + argv[3][7]
+    f.write(text + bd + bm + by + '\n')
+    f.write(text + bm + bd + by + '\n')
+    f.write(text + bm + bd + bys + '\n')
+    f.write(text + bd + bm + bys + '\n')
+    f.write(text + bys + bm + bd + '\n')
+    f.write(text + by + bm + bd + '\n')
+    f.write(bd + bm + by +text +'\n')
+    f.write(bm + bd + by +text +'\n')
+    f.write(bm + bd + bys + text+'\n')
+    f.write(bd + bm + bys + text+'\n')
+    f.write(by + bm + bd + text+'\n')
+    f.write(bys + bm + bd + text+'\n')
+    f.write(by+by+text+'\n')
+    f.write(text+by+by+'\n')
+    f.write(bm+bd+bm+bd+text+'\n')
+    f.write(text+bm+bd+bm+bd+'\n')
+    f.write(text+bd+bm+bd+bm+'\n')
+    f.write(bd+bm+bd+bm+text+'\n')
+    
+
 def main():
-    txtlist = [argv[1],argv[2]]
+    txtlist = [argv[1],argv[2], argv[1] + argv[2]]
     for i in range(len(argv)-4):
         txtlist.append(argv[-i-1])
     for i in range(len(argv[3])):
         try: int(argv[3][i])
         except ValueError: prRed("BIRTHDATE NOT CORRECT FORMAT!"), exit(0)
-            
-    bd = argv[3][0] + argv[3][1]
-    bm = argv[3][2] + argv[3][3]
-    by = argv[3][4] + argv[3][5] + argv[3][6] + argv[3][7]
-    # NAME CREATIONS
-    # name + 1-4 digits
+    if len(argv[3]) > 8: prRed("BIRTHDATE NOT CORRECT FORMAT!"), exit(0)
     for i in range(len(txtlist)):
         txtlist.append(txtlist[i].capitalize())
         txtlist.append(txtlist[i].upper())
@@ -44,10 +69,8 @@ def main():
         for i in range(len(txtlist)):
             f.write(txtlist[i] + '\n')
             fourdigits(txtlist[i], f)
+            addbirth(txtlist[i], f)
         
-# text + bd+bm
-# text + bd+bm+by 
-
 if __name__ == "__main__":
     if len(argv) == 1:
         prRed("Usage: python fkpscrk.py 'NAME' 'SURNAME' 'BIRTHDATE(ddmmyyyy)' 'elsetext'...")
